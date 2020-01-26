2c3844b8375ebc7d73e48bf42ca86b197a69d52c
Davi Holanda <daviht7@gmail.com>
2020-01-26 15:54:09 -0300

    app completo com icone

--- App.js
+++ App.js
@@ -1,106 +1,100 @@
-import React from 'react';
-import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+import React,{ useState } from 'react';
+import { View,StyleSheet, ImageBackground,Text, Button } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+// import { Container } from './styles';
 
-const App = () => {
+export default function App() {
+
+  const [consumido,setConsumido] = useState(0)
+  const [status,setStatus] = useState("Ruim")
+  const [pct,setPct] = useState(0.00)
+  const [meta,] = useState(2000)
+
+  function addCopo() {
+    setConsumido(consumido+200)
+    atualizar()
+  }
+
+  function atualizar() {
+    setPct(parseFloat(pct+10).toFixed(2));
+    setStatus(consumido >= meta ? "Bom" : "Ruim")
+  }
+
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
+    <View style={styles.body}>
+      <ImageBackground style={styles.bgimage} source={require("./images/water.png")}>
+      <View>
+        <View style={styles.infoArea}>
+          <View style={styles.area}>
+            <Text style={styles.texto}>Meta</Text>
+            <Text style={styles.areaDado}>{ `${meta}ml` }</Text>
           </View>
-        </ScrollView>
-      </SafeAreaView>
-    </>
+          <View style={styles.area}>
+          <Text style={styles.texto}>Consumido</Text>
+          <Text style={styles.areaDado}>{ `${consumido}ml` }</Text>
+          </View>
+          <View style={styles.area}>
+            <Text style={styles.texto}>Status</Text>
+            <Text style={styles.areaDado}>{ status }</Text>
+          </View>
+        </View>
+
+        <View style={styles.pctArea}>
+          <Text style={styles.pctText}>{ `${pct}%` }</Text>
+        </View>
+
+        <View style={styles.btnArea}>
+          <Button title="Beber 200ml" onPress={addCopo} />
+        </View>
+
+      </View>
+      
+      </ImageBackground>
+    </View>
   );
-};
+}
 
 const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
-export default App;
+    body : {
+      flex:1,
+      paddingTop: 20
+    },
+    bgimage : {
+      flex: 1,
+      width: null
+    },
+    infoArea : {
+      flex: 1,
+      flexDirection : "row",
+      marginTop: 70
+    },
+    area : {
+      flex: 1,
+      alignItems: "center"
+    },
+    texto : {
+      fontWeight : "bold",
+      fontSize: 20,
+      color: "#45b2fc"
+    },
+    areaDado : {
+      color : "#2b4274",
+      fontSize : 25,
+      fontWeight : "bold"
+    },
+    pctArea : {
+      marginTop: 170,
+      alignItems: "center"
+    },
+    pctText : {
+      fontSize: 70,
+      color : "#2b4274",
+      backgroundColor : "transparent"
+    },
+    btnArea : {
+      marginTop : 30,
+      alignItems : "center"
+    }
+
+})
